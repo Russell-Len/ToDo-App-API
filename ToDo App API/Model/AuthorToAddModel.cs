aae3b0483baee580938ae662fe014fe7bbafc3d5
Russell Len <russell.len@bertelsmann.de>
2023-03-24 13:58:05 +0800

    Added required annotation

--- ToDo App API/Model/AuthorToAddModel.cs
+++ ToDo App API/Model/AuthorToAddModel.cs
@@ -1,8 +1,13 @@
-﻿namespace ToDo_App_API.Model
+﻿using Microsoft.Build.Framework;
+
+namespace ToDo_App_API.Model
 {
     public class AuthorToAddModel
     {
+        [Required]
         public string Username { get; set; } = string.Empty;
+
+        [Required]
         public string Password { get; set; } = string.Empty;
     }
 }
